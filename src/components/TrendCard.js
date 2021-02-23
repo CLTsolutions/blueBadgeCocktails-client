30257d01e7c362781cc703e5c3984a5df54ea23c
Circe Russell <circeroserussell@gmail.com>
2021-02-23 18:24:49 -0500

    Nav bar minor additions

--- src/components/TrendCard.js
+++ src/components/TrendCard.js
@@ -1,16 +1,14 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
-const TrendCard = ({name, id}) => {
+const TrendCard = ({ drinks})  =>{
     return (
-        <div>
-            <ul key={id}>
-                <li>{name}</li>
-            </ul>
+        <div className= "drinkCard">
+            <Link to={`/drinks/${drinks}`}>{drinks}</Link>    
         </div>
-    );
-};
+    )
+}
+
 export default TrendCard;
-
-
 
 
