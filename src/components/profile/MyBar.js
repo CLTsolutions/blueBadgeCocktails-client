3aad013ba9c78077b1b5c77ce2d9fd4a01722769
Chelsey <chelsey.tschida@outlook.com>
2021-02-25 16:49:28 -0500

    successfully fetching search from api

--- src/components/profile/MyBar.js
+++ src/components/profile/MyBar.js
@@ -1,10 +1,11 @@
-import React from 'react'
 import './MyBar.css'
+import SearchCards from '../SearchCards';
 
 const MyBar = () => {
     return (
         <div className='saved-drinks'>
             <h1>My Bar Cart</h1>
+            <SearchCards />
         </div>
     )
 }
