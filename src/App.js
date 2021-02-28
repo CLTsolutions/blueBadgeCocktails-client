9fbda061cbbab2d522a0a4ffb4e64e0f08e93f73
Chelsey <chelsey.tschida@outlook.com>
2021-02-28 12:55:08 -0500

    organized folders and cleaned up navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 // adding css to jsx is that easy
 import './App.css'; // This pattern is preferred where css for this component has a matching .css filename
-import Home from './components/Home'
+import Home from './components/site/Home'
 import MyBar from './components/profile/MyBar';
-import Navbar from './components/Navbar'
-import Splash from './components/Splash';
+import Navbar from './components/site/Navbar'
+import Splash from './components/splash/Splash';
 
 // Defining our <App /> component the function name matches the file name
 function App() {
