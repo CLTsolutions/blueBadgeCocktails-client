b38c0e9222f226912c773643ee5dc27af312277f
Chelsey <chelsey.tschida@outlook.com>
2021-02-28 11:55:48 -0500

    cleaned up myBar page & added light styling

--- src/components/profile/CocktailLibrary.js
+++ src/components/profile/CocktailLibrary.js
@@ -22,15 +22,15 @@ const CocktailLibrary = () => {
     }, [])
 
     return (
-        <div>
+        <div className='flex flex-row'>
             {cocktails.map(cocktail => {
                 return (
-                <div className='border-2 border-indigo-600' key={cocktail.id}>
-                    <h1>{cocktail.name}</h1>
+                <div className='border-2 border-indigo-600 mx-4 my-4' key={cocktail.id}>
+                    <h1 className='mt-4'>{cocktail.name}</h1>
                     <h3>{cocktail.ingredients}</h3>
                     <h3>{cocktail.instructions}</h3>
-                    <button>Update Cocktail</button>
-                    <button>Delete</button>
+                    <button className="focus:outline-none focus:ring-1 focus:ring-gray-900 bg-red-500 hover:bg-red-300 py-2 px-6 mx-2 mt-4 rounded-full shadow-md text-red-200 font-sans">Update</button>
+                    <button className="focus:outline-none focus:ring-1 focus:ring-gray-900 bg-red-500 hover:bg-red-300 py-2 px-6 mx-2 mt-4 rounded-full shadow-md text-red-200 font-sans">Delete</button>
                     <br />
                     <br />
                     <br />
