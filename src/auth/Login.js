615b17ac0adb742b5083e3fd6489a2671a279d13
Chelsey <chelsey.tschida@outlook.com>
2021-03-05 02:06:14 -0500

    committing to grab Zak's changes

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -30,7 +30,7 @@ const Login = (props) => {
                 <div>
                     {/* <label>Email</label> */}
                     <input
-                        className='w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-purple-500'
+                        className='w-full border-2 border-white p-3 rounded outline-none focus:border-purple-500'
                         required
                         type='email'
                         placeholder='Email'
@@ -42,7 +42,7 @@ const Login = (props) => {
                 </div>
                 <div>
                     <input
-                        className='w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-purple-500'
+                        className='w-full border-2 border-white p-3 rounded outline-none focus:border-purple-500'
                         required
                         type='password'
                         placeholder='Password'
@@ -54,7 +54,11 @@ const Login = (props) => {
                 </div>
                 <button 
                     className="flex-shrink-0 bg-purple-500 hover:bg-purple-700 border-purple-500 hover:border-purple-700 text-sm border-4 text-white py-1 px-2 rounded" 
-                    onClick={handleSubmit}>Submit</button>
+                    onClick={handleSubmit}
+                    login={Login}
+                    >
+                Submit
+                </button>
             </form>
         </div>
     );
