f1f077361a6b0e8fe46ca952baa8755bd03479fb
Circe Russell <circeroserussell@gmail.com>
2021-03-04 20:10:37 -0500

    started favorite cocktails file

---
+++ src/components/profile/Favorites.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const favorites=({ setCocktails}) => {
+
+    const [saveCocktail, setSaveCocktail] = useState([])
+    useEffect(()=> {
+        let promiseArr = []
+        favs.map((film) => {
+            promiseArr.push(
+                fetch(film.filmURL)
+                    .then(res => res.json())
+            )
+        })
+        Promise.all(promiseArr).then(results => {
+            console.log(results)
+            setSaveCocktail(results)
+        })
+    }, [favs])
+    console.log(favFilms)
+
+    return (
+        <FavoritesView>
+            {saveCocktail && <h2>Favorites</h2>}
+            {saveCocktail.map(cocktails => {
+                return (
+                    <div key={Cocktail.name}>
+                        <h3>{Cocktail.glassType}&emsp;{Cocktail.ingredients}&emsp;(Cocktail.measurements)&emsp;(Cocktail.instructions)&emsp;()
+                            <StyledButton onClick={() => setCocktails(cocktails)}>Details</StyledButton>
+                        </h3>
+                    </div>
+                )
+            })}
+        </FavoritesView>
+    )
+}
+
+export default Favorites
+
