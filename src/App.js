6e8aa530c30cc56c892b4fa928b5273e4530dba1
Chelsey <chelsey.tschida@outlook.com>
2021-03-07 19:18:10 -0500

    navbar now hidden on auth

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   
   return (
     <div className="App">
-      {!sessionToken ? (
+      {sessionToken ? (
       <Navbar clickLogout={clearToken} token={sessionToken} />
       ) : ( null
       )}
