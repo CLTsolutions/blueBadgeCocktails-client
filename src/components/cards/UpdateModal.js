eb558b2a69aa62f7d81a5f62914aa4433a5fdb16
Circe Russell <circeroserussell@gmail.com>
2021-03-08 19:56:18 -0500

    update card and modal

--- src/components/cards/UpdateModal.js
+++ src/components/cards/UpdateModal.js
@@ -1,23 +1,42 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { Modal, Button } from "antd";
+import UpdateCocktailForm from "./UpdateCocktailForm";
 
-const UpdateModal = ({ updateCocktailForm }) => {
-    
+const UpdateModal = ({ updateCocktail, id, cocktail, fetchDrinks }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-    return (
-      <>
-        
-        <Modal
-          title="Update your cocktail."
-          visible={isModalVisible}
-          onOk={handleOk}
-          onCancel={handleCancel}
-        >
-          <p>Working?</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-        </Modal>
-      </>
-    );
-}
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal
+        title="Update your cocktail."
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <UpdateCocktailForm
+          updateCocktail={updateCocktail}
+          cocktail={cocktail}
+          id={cocktail.id}
+          fetchDrinks={fetchDrinks}
+        />
+      </Modal>
+    </>
+  );
+};
 
 export default UpdateModal;
